7d77c7e369bb9462d8b43a5b503e868cfc59a735
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-04-26 20:56:42 +0530

    carousel and update product added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  currentSlide = 0;
 
+  prevSlide() {
+    this.currentSlide = (this.currentSlide === 0) ? 2 : this.currentSlide - 1;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide === 2) ? 0 : this.currentSlide + 1;
+  }
 }
--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { product } from '../data-types';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-seller-update-product',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule],
   templateUrl: './seller-update-product.component.html',
-  styleUrl: './seller-update-product.component.css'
+  styleUrl: './seller-update-product.component.css',
 })
-export class SellerUpdateProductComponent {
-  addproduct(data:any){
+export class SellerUpdateProductComponent implements OnInit {
+  productData: undefined | product;
+  productMessage: undefined | string;
+  productMessage2: undefined | string;
+  constructor(private route: ActivatedRoute, private product: ProductService,private router  : Router) {}
+  ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    productId &&
+      this.product.getProduct(productId).subscribe((data) => {
+        console.warn(data);
+        this.productData = data;
+      });
+  }
+  addproduct(data: product) {
+    console.warn(data);
+    if(this.productData){
+      data.id = this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product has beeen Updated';
+        this.productMessage2= "Page will be redirected after 3 seconds"
 
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+      this.productMessage2 = undefined;
+      this.router.navigate(['/seller-home'])
+    }, 3000);
   }
 }
