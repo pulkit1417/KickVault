05d985395bc89f5e9841c34322bf9555c876d341
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-06-04 20:23:35 +0530

    cart functionality added remotely

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
-import { Login, SignUp } from '../data-types';
+import { cart, Login, product, SignUp } from '../data-types';
 import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -13,8 +14,8 @@ import { UserService } from '../services/user.service';
 })
 export class UserAuthComponent implements OnInit {
   showLogin: boolean = true;
-  authError:string="";
-  constructor(private user: UserService) {}
+  authError: string = '';
+  constructor(private user: UserService, private product: ProductService) {}
   ngOnInit(): void {
     this.user.userAuthReload();
   }
@@ -23,17 +24,48 @@ export class UserAuthComponent implements OnInit {
   }
   openLogin() {
     this.showLogin = false;
-    this.user.inValidUserAuth.subscribe((result)=>{
-      console.warn("apple",result);
-      if(result){
-        this.authError="Please Enter Valid User Credentials"
-      }
-    })
   }
   signUp(data: SignUp) {
     this.user.userSignUp(data);
   }
   login(data: Login) {
     this.user.userLogin(data);
+    this.user.inValidUserAuth.subscribe((result) => {
+      console.warn('apple', result);
+      if (result) {
+        this.authError = 'Please Enter Valid User Credentials';
+      }
+      else{
+        this.localCartToRemoteCart();
+      }
+    });
   }
+  localCartToRemoteCart() {
+    let data = localStorage.getItem('LocalCart');
+    if (data) {
+      let cartDataList: product[] = JSON.parse(data);
+      let user = localStorage.getItem('user');
+      let userId = user && JSON.parse(user).id;
+
+      cartDataList.forEach((product: product,index) => {
+        let cartData: cart = {
+          ...product,
+          productId: product.id,
+          userId,
+        };
+
+        delete cartData.id;
+        setTimeout(() => {
+          this.product.addToCart(cartData).subscribe((result)=>{
+            if(result){
+              console.warn("Item is stored in DB");
+            }
+          })
+        }, 500);
+        if(cartDataList.length===index+1){
+          localStorage.removeItem('LocalCart')
+        }
+      })
+     }
+    }  
 }
